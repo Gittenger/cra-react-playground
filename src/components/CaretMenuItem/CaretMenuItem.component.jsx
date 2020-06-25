7e1b11394faa713918deca3c6619084ce0de6313
gittenger <john.pittenger86@gmail.com>
2020-06-24 23:41:02 -0400

    dynamic caret svg working

--- src/components/CaretMenuItem/CaretMenuItem.component.jsx
+++ src/components/CaretMenuItem/CaretMenuItem.component.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 
+import { ReactComponent as CaretClosed } from "../../assets/caret-right.svg";
+import { ReactComponent as CaretOpen } from "../../assets/caret-down.svg";
 import { CaretMenuItemContainer } from "./CaretMenuItem.styles.jsx";
 
 const CaretMenuItem = () => {
   const [menuActive, setMenuActive] = useState(false);
 
+  const Caret = (function () {
+    return menuActive ? CaretOpen : CaretClosed;
+  })();
+
   return (
     <CaretMenuItemContainer className={menuActive ? "active" : ""}>
+      <Caret />
       <button onClick={() => setMenuActive(!menuActive)}>
         <h4>Title</h4>
       </button>
